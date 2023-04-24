7506f83b0928053ab9931baee6b2b4f14085fe59
Rafael Borba <36474347+rafaelborbacs@users.noreply.github.com>
2023-04-24 07:20:46 -0300

    initial commits

--- files.js
+++ files.js
@@ -1,14 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 
+const shuffle = (arr) => arr.sort(() => Math.random() - 0.5)
+
 const getSCPFiles = async (self) => {
-    return fs.readdirSync(self.scpfolder).filter(file => {
-        const filePath = path.join(self.scpfolder, file)
-        return !file.endsWith(".part")
-            && !file.startsWith(".")
-            && fs.existsSync(filePath)
-            && fs.statSync(filePath).isFile()
-    })
+    return shuffle(
+        fs.readdirSync(self.scpfolder).filter(file => {
+            const filePath = path.join(self.scpfolder, file)
+            return !file.endsWith(".part")
+                && !file.startsWith(".")
+                && fs.existsSync(filePath)
+                && fs.statSync(filePath).isFile()
+        })
+    )
 }
 
 module.exports = getSCPFiles
