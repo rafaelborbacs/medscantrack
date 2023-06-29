c2a90fb76f3d44adfb4c73fe7bec45d321c1e9b6
Rafael Borba <36474347+rafaelborbacs@users.noreply.github.com>
2023-06-29 13:48:53 -0300

    Revert "Fix"
    This reverts commit 68e4104d3215d4198cad6c3e7103c2214144d2d1.

--- scpfiles.js
+++ scpfiles.js
@@ -5,11 +5,11 @@ const getSCPFiles = (checkUse) => new Promise(resolve => {
     fs.readdir(process.self.scpfolder, (err, files) => {
         resolve(
             files.filter(async file => {
-                if(!file.endsWith('.part') && !file.endsWith('.tmp') && !file.startsWith('.')){
-                    const filePath = path.join(process.self.scpfolder, file)
-                    const stats = await new Promise(resolve => fs.stat(filePath, (err, stat) => resolve(stat)))
-                    return stats && stats.isFile() && (!checkUse || (Date.now() - stats.atimeMs) > 5000)
-                }
+                if(file.endsWith(".part") || file.endsWith(".tmp") || file.startsWith("."))
+                    return false
+                const filePath = path.join(process.self.scpfolder, file)
+                const stats = await new Promise(resolve => fs.stat(filePath, (err, stat) => resolve(stat)))
+                return stats && stats.isFile() && (!checkUse || (Date.now() - stats.atimeMs) > 5000)
             })
         )
     })
