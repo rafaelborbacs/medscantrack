18ff998ee381557a2ed558a93d245f88542c6cb4
Rafael Borba <36474347+rafaelborbacs@users.noreply.github.com>
2023-06-29 13:23:28 -0300

    Fix

--- scpfiles.js
+++ scpfiles.js
@@ -4,10 +4,10 @@ const path = require('path')
 const getSCPFiles = (checkUse) => new Promise(resolve => {
     fs.readdir(process.self.scpfolder, (err, files) => {
         resolve(
-            files.filter(async file => {
+            files.filter(file => {
                 if(!file.endsWith('.part') && !file.endsWith('.tmp') && !file.startsWith('.')){
                     const filePath = path.join(process.self.scpfolder, file)
-                    const stats = await new Promise(resolve => fs.stat(filePath, (err, stat) => resolve(stat)))
+                    const stats = fs.statSync(filePath)
                     return stats && stats.isFile() && (!checkUse || (Date.now() - stats.atimeMs) > 5000)
                 }
             })
