106e12008a47ee2593ce63f271d3d9532b1384a0
Rafael Borba <36474347+rafaelborbacs@users.noreply.github.com>
2023-06-29 18:36:36 -0300

    Feature

--- scpfiles.js
+++ scpfiles.js
@@ -1,14 +1,24 @@
 const fs = require('fs')
 const path = require('path')
 
-const getSCPFiles = (checkUse) => {
-    return fs.readdirSync(process.self.scpfolder).filter(file => {
-        const filePath = path.join(process.self.scpfolder, file)
-        if(file.endsWith(".part") || file.endsWith(".tmp") || file.startsWith(".") || !fs.existsSync(filePath))
-            return false
-        const stats = fs.statSync(filePath)
-        return stats && stats.isFile() && (!checkUse || (Date.now() - stats.atimeMs) > 5000)
+const getSCPFiles = (checkUse) => new Promise(resolve => {
+    const filtered = []
+    const calls = new Set()
+    fs.readdir(process.self.scpfolder, (err, files) => {
+        files.forEach(file => {
+            if(!file.endsWith('.part') && !file.endsWith('.tmp') && !file.startsWith('.')){
+                const filePath = path.join(process.self.scpfolder, file)
+                calls.add(file)
+                fs.stat(filePath, (err, stats) => {
+                    if(!err && stats && stats.isFile() && (!checkUse || (Date.now() - stats.atimeMs) > 5000))
+                        filtered.push(file)
+                    calls.delete(file)
+                    if(calls.size === 0)
+                        resolve(filtered)
+                })
+            }
+        })
     })
-}
+})
 
 module.exports = { getSCPFiles }
